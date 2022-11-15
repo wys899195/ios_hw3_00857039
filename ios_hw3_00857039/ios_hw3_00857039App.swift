b069463942d921b7dd891df477913dfce3b42b02
steven69 <steven62885@gmail.com>
2022-11-15 15:06:52 +0800

    Initial Commit

---
+++ ios_hw3_00857039/ios_hw3_00857039App.swift
@@ -0,0 +1,17 @@
+//
+//  ios_hw3_00857039App.swift
+//  ios_hw3_00857039
+//
+//  Created by User20 on 2022/11/15.
+//
+
+import SwiftUI
+
+@main
+struct ios_hw3_00857039App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
